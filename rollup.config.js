1818bd1b9be8b796d821ccbb3e0358f2d43854e0
Jani Eränne <jani.eranne@aalto.fi>
2024-02-21 15:29:11 +0200

    Bugfixes

--- rollup.config.js
+++ rollup.config.js
@@ -25,10 +25,11 @@ export default [
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
     ],
+    external: ["react", "react-dom"],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts.default()],
+    plugins: [dts.default()]
   },
 ];
